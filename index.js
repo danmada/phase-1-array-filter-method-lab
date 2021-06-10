754cf846052e1f442b50085987f7f44b257e93c7
danmada <danmada@gmail.com>
2021-06-10 18:13:22 -0500

    commit tree 53317f23464a448fab1c77bf58a355b647e1b06e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, driverName) {
+    return array.filter(drivers => 
+        drivers.toLowerCase() === driverName.toLowerCase()
+        )
+}
+   
+function fuzzyMatch(array, name) {
+    return array.filter(driver => 
+        driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+        )
+}
+
+ 
+function matchName(array, name) {
+    return array.filter(driverAndName => 
+        driverAndName.name === name)
+}
